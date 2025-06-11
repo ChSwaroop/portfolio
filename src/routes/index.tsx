a5b62320c1ffe752103dc23f41e4b64721fb19e4
swaroop <swaroop.chitturi@apxor.com>
2025-06-11 20:23:57 +0530

    feat: made initial landing screen with details

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
-
-import { CardBody, CardContainer, CardItem } from '@/components/ui/3d-card';
+import Projects from '@/components/Projects';
+import { Timeline } from '@/components/ui/timeline';
 import { createFileRoute } from '@tanstack/react-router'
 import { Code, Github, Linkedin, Mail } from 'lucide-react';
 import { motion } from 'motion/react';
@@ -11,17 +11,64 @@ export const Route = createFileRoute('/')({
 
 function RouteComponent() {
 
+  const data = [
+    {
+      title: "Apr 2025 - Present",
+      content: (
+        <div>
+          <p className="text-base font-normal text-neutral-800 md:text-xl capitalize dark:text-neutral-200">
+            Apxor technology solutions
+          </p>
+          <p className='pb-8 text-sm md:text-lg text-muted-foreground'>Associate SDE</p>
+          <ul className='text-sm list-disc space-y-4 md:text-base'>
+            <li>Gaining hands-on experience with full-stack technologies, primarily focusing on React for frontend and Go for backend
+              services, while building scalable web applications and interactive user interfaces.</li>
+            <li>Actively learning about Large Language Models (LLMs), AI-driven tools, and integrating cutting-edge AI technologies
+              into software solutions to enhance user experiences and product functionalities.</li>
+          </ul>
+        </div>
+      ),
+    },
+    {
+      title: "Oct 2024 – Apr 2025",
+      content: (
+        <div>
+          <p className="text-base font-normal text-neutral-800 md:text-xl capitalize dark:text-neutral-200">
+            Apxor technology solutions
+          </p>
+          <p className='pb-8 text-sm md:text-lg text-muted-foreground'>Associate SDE Intern</p>
+          <ul className='text-sm list-disc space-y-4 md:text-base'>
+            <li>Gaining hands-on experience with full-stack technologies, primarily focusing on React for frontend and Go for backend
+              services, while building scalable web applications and interactive user interfaces.</li>
+            <li>Actively learning about Large Language Models (LLMs), AI-driven tools, and integrating cutting-edge AI technologies
+              into software solutions to enhance user experiences and product functionalities.</li>
+          </ul>
+        </div>
+      ),
+    },
+    {
+      title: "Apr 2024 – Sep 2024",
+      content: (
+        <div>
+          <p className="text-base font-normal text-neutral-800 md:text-xl capitalize dark:text-neutral-200">
+            Workbees technologies
+          </p>
+          <p className='pb-8 text-sm md:text-lg text-muted-foreground'>Associate SDE Intern</p>
+          <ul className='text-sm list-disc space-y-4 md:text-base'>
+            <li>As a Flutter development intern, I actively involved in developing mobile applications using Flutter framework, including
+              UI design, implementation of features, and integration of backend services.</li>
+            <li>Engaged in collaborative efforts with senior developers and designers to understand project requirements, troubleshoot
+              issues, and implement effective solutions.</li>
+          </ul>
+        </div>
+      ),
+    },
+  ];
 
-  return <div className='flex flex-col min-h-screen w-full items-center'>
-
-
+  return <div className='flex flex-col min-h-screen w-full items-center pt-20'>
     <div className='flex flex-col items-center justify-start h-full w-full overflow-hidden relative box-border'>
-
-
-
       <div className='min-h-[400px] w-full relative flex justify-center' >
-        <motion.div className='h-[85%] w-[270px] absolute bg-linear-to-b from-muted via-muted to-background rounded-t-full flex justify-center z-0 bottom-0'
-
+        <motion.div className='h-[85%] w-[270px] absolute bg-linear-to-b/decreasing from-gray-300 dark:from-muted to-background rounded-t-full flex justify-center z-0 bottom-0'
           initial={{
             y: -50,
             opacity: 0,
@@ -35,7 +82,7 @@ function RouteComponent() {
             ease: 'easeIn'
           }}
         />
-        <motion.img src="/assets/images/hero.png" alt="hero" className='h-full max-w-full absolute z-1 bottom-0 mr-3'
+        <motion.img src="/assets/images/hero.png" alt="hero" className='h-full max-w-full absolute z-1 bottom-0 mr-3 drop-shadow-lg drop-shadow-muted-foreground dark:drop-shadow-none'
           initial={{
             y: 50,
             opacity: 0,
@@ -52,7 +99,7 @@ function RouteComponent() {
         />
         {/* <div className='bg-muted h-[80%] w-64 right-0 absolute rounded-bl-[50%] rounded-br-[50%]' /> */}
       </div>
-      <motion.h1 className='text-4xl md:text-6xl font-medium uppercase text-white/90 z-20 text-center'
+      <motion.h1 className='text-4xl md:text-6xl font-medium uppercase z-20 text-center'
         initial={{
           y: -10,
         }}
@@ -103,110 +150,10 @@ function RouteComponent() {
       </ul>
     </motion.footer>
 
-    <div className='w-full flex flex-wrap md:flex-nowrap justify-evenly px-5 items-center'>
-      <CardContainer className="inter-var place-content-start">
-        <CardBody className=" bg-gray-50 group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] md:max-w-[60vh] lg:max-w-full h-auto rounded-2xl p-6 border  ">
-          <CardItem
-            translateZ="50"
-            className="text-xl font-bold text-black dark:text-white"
-          >
-            Aditya App
-          </CardItem>
-          <CardItem
-            as="p"
-            translateZ="60"
-            className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-          >
-            Dart , Flutter , Firebase
-          </CardItem>
-          <CardItem translateZ="100" className="w-full mt-4">
-            <img
-              src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              height="800"
-              width="800"
-              className="h-60 w-full object-cover rounded-2xl group-hover/card:shadow-xl"
-              alt="thumbnail"
-            />
-          </CardItem>
-          <div className="flex justify-end items-center mt-10">
-            {/* <CardItem
-                            translateZ={20}
-                            as="a"
-                            href="https://twitter.com/mannupaaji"
-                            target="__blank"
-                            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-                        >
-                            Try now →
-                        </CardItem> */}
-            <CardItem
-              translateZ={20}
-              as="button"
-              className="px-4 py-2 rounded-2xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
-            >
-              Sign up
-            </CardItem>
-          </div>
-        </CardBody>
-      </CardContainer>
-      <div>
-        <ul className='ml-8 list-disc text-lg' >
-          <li>Developed a user-friendly application using the Flutter framework and Firebase database.</li>
-          <li>Used Firebase for user authentication and storing the reviews of the users.</li>
-        </ul>
-      </div>
-    </div>
-    <div className='w-full flex justify-evenly px-10 items-center'>
-      <CardContainer className="inter-var place-content-start">
-        <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-2xl p-6 border  ">
-          <CardItem
-            translateZ="50"
-            className="text-xl font-bold text-black dark:text-white"
-          >
-            2. Aditya App
-          </CardItem>
-          <CardItem
-            as="p"
-            translateZ="60"
-            className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-          >
-            Dart , Flutter , Firebase
-          </CardItem>
-          <CardItem translateZ="100" className="w-full mt-4">
-            <img
-              // src="/assets/images/1.jpg"
-              src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              // height="800"
-              // width="800"
-              className="h-60 w-full object-cover rounded-2xl group-hover/card:shadow-xl"
-              alt="thumbnail"
-            />
-          </CardItem>
-          <div className="flex justify-end items-center mt-10">
-            {/* <CardItem
-                            translateZ={20}
-                            as="a"
-                            href="https://twitter.com/mannupaaji"
-                            target="__blank"
-                            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-                        >
-                            Try now →
-                        </CardItem> */}
-            <CardItem
-              translateZ={20}
-              as="button"
-              className="px-4 py-2 rounded-2xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
-            >
-              Sign up
-            </CardItem>
-          </div>
-        </CardBody>
-      </CardContainer>
-      <div className=''>
-        <ul className='list-disc' >
-          <li>Developed a user-friendly application using the Flutter framework and Firebase database.</li>
-          <li>Used Firebase for user authentication and storing the reviews of the users.</li>
-        </ul>
-      </div>
-    </div>
+    <Projects />
+
+    <Timeline data={data} />
+
+    <div className='h-screen w-screen'></div>
   </div >
 }
